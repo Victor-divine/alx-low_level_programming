31b7a679f622cde6d6131cf5dfdf1c72acb60b2e
sirvic <sirvic.chuks@gmail.com>
2023-02-27 06:44:32 -0800

    c pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
 /**
- * reset_to_98 - update the value of a var using pointers.
- * @n: int to update.
- * Return: 0
- */
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Owned By vic tech.
+*  Return: 0 is success
+*/
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
