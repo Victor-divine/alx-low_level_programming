f927efafad464dffed615047bab87a2360d6dd0d
sirvic <sirvic.chuks@gmail.com>
2023-03-01 21:33:12 -0800

    c pointer

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+0x06. C - More pointers, arrays and strings
+
+0. strcat
+1. strncat
+2. strncpy
+3. strcmp
+4. I am a kind of paranoid in reverse. I suspect people of plotting to make me happy
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void print_message(void)
+{
+        printf("_putchar\n");
+        return;
+}
+
+void print_alphabet()
+{
+        char min = 'a';
+        while (min <= 'a')
+        {
+                printf("%c", min);
+                min++;
+        }
+}
+~
+~
+
