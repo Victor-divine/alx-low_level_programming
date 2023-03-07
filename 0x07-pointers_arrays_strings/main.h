da94d51f4da159a1e3aa67d35356239d5cae3bc2
sirvic <sirvic.chuks@gmail.com>
2023-03-06 21:19:12 -0800

    c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
 
 
 #include <stdio.h>
@@ -13,4 +13,5 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 #endif/*MAIN_H*/
